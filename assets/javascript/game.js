378d137d7796e8c43e232dc7472b3db753901aa7
Mark Silva <marksilva@D-10-19-116-199.dhcp4.washington.edu>
2019-03-20 17:17:09 -0700

    jQuery updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,19 +13,46 @@ var wins = 0;
 var losses = 0;
 var userScore = 0;
 //generate a random number from 19 - 120
-var randomNumber = Math.floor(Math.random() * 101) + 19;
+var randomNumber = Math.floor(Math.random() * 102) + 19;
 console.log(randomNumber);
-
-
-//genrate a random number for cyrstals from 1 - 12
-var crystalNumber = Math.floor(Math.random() * 12);
-console.log(crystalNumber);
-
-
-
 
 
 winCount.textContent = wins;
 lossCount.textContent = losses;
 computerNumber.textContent = randomNumber;
 userCount.textContent = userScore;
+
+
+$(document).ready(() => {
+
+    //Give each crystal a random number
+    $('.img').each(function () {
+        //genrate a random number for cyrstals from 1 - 12
+        var crystalNumber = Math.floor(Math.random() * 12) + 1;
+        console.log(crystalNumber);
+        $(this).attr('data-crystalNumber', crystalNumber);
+    });
+    
+    //When a crystal is clicked, the number is added to playerScore
+    $('.img').on('click', function() {
+        // console.log($(this).attr('data-crystalNumber'));
+        var num = parseInt($(this).attr('data-crystalNumber'));
+        userScore += num;
+        console.log(userScore);
+        $('#playerScore').text(userScore);
+        
+       //If playerScore matches randomNumber, player is alerted of win and winCount goes up
+        if (userScore === randomNumber) {
+            wins ++;
+            alert("You Win!"); 
+        //If playerScore exceeds randonNumber, player is alerted of loss and lossCount goes up
+        } else if (userScore > randomNumber) {
+            losses ++;
+            alert("You Lose!"); 
+        }
+
+    });
+
+    //Game resets
+
+});
